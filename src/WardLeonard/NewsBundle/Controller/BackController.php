4801d47cfd6cd48d10d8807bc00887091f9f0c85
Eric Madrange <ericmadrange@gmail.com>
2017-11-27 12:47:51 +0100

    Création de l'entité news

---
+++ src/WardLeonard/NewsBundle/Controller/BackController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace WardLeonard\NewsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class BackController extends Controller
+{
+    /**
+     * @Route("/admin")
+     */
+    public function indexAction()
+    {
+        return $this->render('WardLeonardNewsBundle:Back:index.html.twig', array(
+            // ...
+        ));
+    }
+
+}
---
+++ src/WardLeonard/NewsBundle/Entity/News.php
@@ -0,0 +1,260 @@
+<?php
+
+namespace WardLeonard\NewsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * News
+ *
+ * @ORM\Table(name="news")
+ * @ORM\Entity(repositoryClass="WardLeonard\NewsBundle\Repository\NewsRepository")
+ */
+class News
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255, nullable=true)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="video", type="string", length=255, nullable=true)
+     */
+    private $video;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="photo", type="string", length=255, nullable=true)
+     */
+    private $photo;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_publication", type="datetime")
+     */
+    private $datepublication;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="publish", type="boolean")
+     */
+    private $publish;
+
+    /**
+     * News constructor.
+     */
+    public function __construct()
+    {
+        $this->datepublication = new \DateTime();
+        $this->publish = true;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return News
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return News
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return News
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set video
+     *
+     * @param string $video
+     *
+     * @return News
+     */
+    public function setVideo($video)
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+
+    /**
+     * Get video
+     *
+     * @return string
+     */
+    public function getVideo()
+    {
+        return $this->video;
+    }
+
+    /**
+     * Set photo
+     *
+     * @param string $photo
+     *
+     * @return News
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get photo
+     *
+     * @return string
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * Set datepublication
+     *
+     * @param \DateTime $datepublication
+     *
+     * @return News
+     */
+    public function setDatepublication($datepublication)
+    {
+        $this->datepublication = $datepublication;
+
+        return $this;
+    }
+
+    /**
+     * Get datepublication
+     *
+     * @return \DateTime
+     */
+    public function getDatepublication()
+    {
+        return $this->datepublication;
+    }
+
+    /**
+     * Set publish
+     *
+     * @param boolean $publish
+     *
+     * @return News
+     */
+    public function setPublish($publish)
+    {
+        $this->publish = $publish;
+
+        return $this;
+    }
+
+    /**
+     * Get publish
+     *
+     * @return bool
+     */
+    public function getPublish()
+    {
+        return $this->publish;
+    }
+}
+
