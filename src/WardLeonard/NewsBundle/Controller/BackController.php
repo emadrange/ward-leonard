2e6703e9ccd387493c5d83399ae67a76e07892dc
Eric Madrange <ericmadrange@gmail.com>
2017-11-28 17:22:01 +0100

    Création de la method modifyAction

--- src/WardLeonard/NewsBundle/Controller/BackController.php
+++ src/WardLeonard/NewsBundle/Controller/BackController.php
@@ -4,7 +4,6 @@ namespace WardLeonard\NewsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,17 +20,17 @@ class BackController extends Controller
      */
     public function indexAction()
     {
-
         $repository = $this->getDoctrine()->getManager()->getRepository("WardLeonardNewsBundle:News");
         $news = $repository->findAll();
 
-        return $this->render('WardLeonardNewsBundle:Back:index.html.twig', array('news' => $news
-        ));
+        return $this->render('WardLeonardNewsBundle:Back:index.html.twig', array('news' => $news));
     }
 
     /**
      * @Route("/admin/add", name="back_news_add")
      * @Template
+     * @param Request $request
+     * @return array
      */
     public function addAction(Request $request){
         $form = $this->createForm(NewsType::class, new News(), array(
@@ -62,7 +61,7 @@ class BackController extends Controller
             $news->setPhoto($filePhoto->getClientOriginalName());
             $em->persist($news);
             $em->flush();
-            $filePhoto->move($this->getParameter('image_path'), $filePhoto->getClientOriginalName() );
+            $filePhoto->move($this->getParameter('image_path'), $filePhoto->getClientOriginalName());
 
             //$this->redirect($this->generateUrl('back_news_add'));
         }
@@ -71,19 +70,59 @@ class BackController extends Controller
              'form_add' => $form->createView()
          ));*/
 
-         return array('form_add' => $form->createView());
+         return array('form_add' => $form->createView(), 'titre' => 'Ajouter une news');
          // redirige vers add.html.twig qui récupère form_add
     }
 
+    /**
+     * @Route("/admin/modify/{id}", name="back_news_modify")
+     * @param Request $request
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function modifyAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $news = $em->getRepository('WardLeonardNewsBundle:News')->find($id);
+
+        $form = $this->createForm(NewsType::class, $news, array(
+
+        ))
+            ->add('submit', SubmitType::class, array(
+                'label' => 'form.news.save'
+            ))
+            ->add('reset', ResetType::class, array(
+                'label' => 'form.news.reset'
+            ));
+
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST') && $form->isValid()) {
+            $news = $form->getData();
+            $filePhoto = $form['photo']->getData();
+
+            $news->setPhoto($filePhoto->getClientOriginalName());
+            $em->persist($news);
+            $em->flush();
+            $filePhoto->move($this->getParameter('image_path'), $filePhoto->getClientOriginalName());
+
+            return $this->redirect($this->generateUrl('back_news_liste'));
+        }
+        return $this->render('WardLeonardNewsBundle:Back:add.html.twig', array(
+            'form_add' => $form->createView(),
+            'titre' => "Modification d'une news"));
+    }
 
     /**
      * @param Request $request
      * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
      * @Route("/admin/delete/{id}", name="back_news_delete")
+     *
      */
     public function deleteAction(Request $request, $id)
     {
-
         $em = $this->getDoctrine()->getManager();
         $repository = $em->getRepository("WardLeonardNewsBundle:News");
 
